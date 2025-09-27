f742209841034118bffc6444210b406094c3f315
Takashi Yamashina <takashi.yamashina@gmail.com>
2025-09-27 22:44:48 +0900

    test: use vitest-v8

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: ["./docs/**", "./specs/**", "./node_modules/**"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov", "html"],
+      exclude: [
+        "**/specs/**",
+        "**/docs/**",
+        "**/dist/**",
+        "**/tests/**",
+        "**/node_modules/**",
+        "**/vitest.config.ts",
+        "**/eslint.config.js",
+        "**/tsup.config.ts",
+        "**/src/types.ts",
+        "**/src/i18n/**",
+      ],
+    },
+  },
+});
